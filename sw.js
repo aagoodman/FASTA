bbc26a6b7f61db9b8b8be38cb0fe8273445d8604
Alyssa <agoodman@cfa.harvard.edu>
2025-08-25 00:42:42 -0400

    Update sw.js again

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-const CACHE = "fasta-v8";   // <- bump this
+// sw.js
+const CACHE = "fasta-v9";     // <-- bump this
 const ASSETS = [
   "./",
-  "./index.html",
+  "./abbyapp.html",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/icon-1024.png",
-  "./icons/splash-1284x2778.png"
+  "./icons/splash-1284x2778.png" // <- use your real filename here
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
@@ -21,6 +22,6 @@ self.addEventListener("activate", (e) => {
   );
   self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
 });
