43e8e292f32c854252f76027b91f0fe808213ebc
Alyssa <agoodman@cfa.harvard.edu>
2025-08-28 10:34:36 -0400

    Create version.js

---
+++ version.js
@@ -0,0 +1,4 @@
+// version.js
+// One place to bump on each release:
+var FASTA_RELEASE = '1.0.5';        // what you show on the splash
+var FASTA_CACHE   = 'fasta-v1.0.5'; // what the SW uses for its cache key
